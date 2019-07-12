63768fc5665225b26ab3dc5aa05fee1e7d5e2f49
sha5hwati <shashwatishradha5@gmail.com>
2019-07-12 10:53:19 -0600

    Adding table feature

--- transact_write_items.go
+++ transact_write_items.go
@@ -22,20 +22,21 @@ func (e *MockDynamoDB) TransactWriteItems(input *dynamodb.TransactWriteItemsInpu
 	if len(e.dynaMock.TransactWriteItemsExpect) > 0 {
 		x := e.dynaMock.TransactWriteItemsExpect[0] //get first element of expectation
 
-		/*foundTable := false
+		foundTable := false
 		if x.table != nil {
 			for _, item := range input.TransactItems {
 				if (item.Update != nil && *x.table == *item.Update.TableName) ||
 					(item.Put != nil && *x.table == *item.Put.TableName) ||
 					(item.Delete != nil && *x.table == *item.Delete.TableName) {
 					foundTable = true
+					break
 				}
 			}
 
 			if foundTable == false {
 				return nil, fmt.Errorf("Expect table %s not found", *x.table)
 			}
-		}*/
+		}
 
 		// delete first element of expectation
 		e.dynaMock.TransactWriteItemsExpect = append(e.dynaMock.TransactWriteItemsExpect[:0],
